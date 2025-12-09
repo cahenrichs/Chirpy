43a88b92ffdf7918c1e64c2e4e0363940d9ff4a3
cahenrichs <cahenrichs@gmail.com>
2025-12-09 20:18:25 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server {
+		Addr:	":8080",
+		Handler: mux,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
