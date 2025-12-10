31cc2064b1e785c130c393c28efe13a1c6be99fa
cahenrichs <cahenrichs@gmail.com>
2025-12-10 22:40:29 +0000

    made handlerReadines

--- main.go
+++ main.go
@@ -2,19 +2,29 @@ package main
 
 import (
 "net/http"
+"log"
 )
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+	}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix( "/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/assets", http.FileServer(http.Dir(filepathRoot)))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
