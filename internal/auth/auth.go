c489f0cb38d11db7908edea4007dc9232e2fa380
cahenrichs <cahenrichs@gmail.com>
2025-12-29 18:21:19 +0000

    added the MakeJWT func

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -13,3 +13,15 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 	match, err := argon2id.ComparePasswordAndHash(password, hash)
 	return match, err
 }
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	signingKey := []byte(tokenSecret)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		IssuedAt: jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject: userID.String(),
+	})
+
+	return token.SignedString(signingKey)
+}
